0a508d96e755f2d729f819d967b72ba1e679a632
mohameddwedar689 <mohameddwedar689@gmail.com>
2023-12-05 16:08:49 +0200

    [create] database models

--- tickests/models.py
+++ tickests/models.py
@@ -1,3 +1,21 @@
 from django.db import models
 
 # Create your models here.
+'''
+Busniess logic
+    Guest <-> Movie -> Reservation
+'''
+
+class Movie(models.Model):
+    hall = models.CharField(max_length=10)
+    movie = models.CharField(max_length=30)
+    date = models.DateField()
+
+class Guest(models.Model):
+    name = models.CharField(max_length=30)
+    mobile = models.CharField(max_length=12)
+    
+class Reservation(models.Model):
+    guest = models.ForeignKey(Guest , related_name='reservation' , on_delete=models.CASCADE)
+    movie = models.ForeignKey(Movie , related_name='reservation' , on_delete=models.CASCADE)
+    
